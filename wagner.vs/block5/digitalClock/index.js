7596bf3030ccff0d35ca51ad5384d8e90c7cc0e3
tis a secret <125412609+J3inx@users.noreply.github.com>
2024-12-18 13:02:31 -0500

    Add files via upload

---
+++ wagner.vs/block5/digitalClock/index.js
@@ -0,0 +1,16 @@
+//this is the digital clock program
+function updateClock(){
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const miliseconds = now.getMilliseconds().toString().padStart(2,0);
+
+   // console.log(hours+":" + minutes + ":" + seconds+ ":" + miliseconds);
+    const timeString = `${hours}:${minutes}:${seconds}:${miliseconds}` //use the non shift tilda to do this instead of single quotes
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1);
+//while(true){updateClock();}
