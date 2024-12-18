afb5155a16560cf9a7e6bc7f2416e20613e7fa20
tis a secret <125412609+J3inx@users.noreply.github.com>
2024-12-18 14:24:21 -0500

    second test

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-//this is the digital clock program
-function updateClock(){
+function updateClock() {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const seconds = now.getSeconds().toString().padStart(2,0);
-    const miliseconds = now.getMilliseconds().toString().padStart(3,0);
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const milliseconds = now.getMilliseconds().toString().padStart(3, '0');
 
-   // console.log(hours+":" + minutes + ":" + seconds+ ":" + miliseconds);
-    const timeString = `${hours}:${minutes}:${seconds}:${miliseconds}` //use the non shift tilda to do this instead of single quotes
+    const timeString = `${hours}:${minutes}:${seconds}:${milliseconds}`;
     document.getElementById("clock").textContent = timeString;
 }
 
+// Use setInterval to update every 100 milliseconds instead of 1 millisecond
 updateClock();
 setInterval(updateClock, 100);
-//while(true){updateClock();}
