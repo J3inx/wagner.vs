4c2c720750d26317dd4f15a6e87de95f58a996c7
tis a secret <125412609+J3inx@users.noreply.github.com>
2025-01-16 13:19:25 -0500

    working on it

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-import requests
-import asyncio
 import sys
 import os
 sys.path.insert(0, os.path.expanduser('~/Desktop/wagner.vs/modules'))
+custom_modules_path = os.path.expanduser("desktop/wagner.vs/modules")
+if custom_modules_path not in sys.path:
+    sys.path.append(custom_modules_path)
+import requests
+import asyncio
+import dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+print(os.environ['PATH'])
+
 
 # ProxyCrawl API URL (replace with actual API key)
 PROXY_API_URL = "http://iLxrCSDSSvZzp9ug7f_TjA@smartproxy.crawlbase.com:8012"
