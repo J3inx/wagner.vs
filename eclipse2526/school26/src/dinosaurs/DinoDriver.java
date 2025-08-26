ce6a2a748eb1b7bf95b7b06b30167e22a90e9778
J3inx <secret.atent.man@gmail.com>
2025-08-26 15:01:02 -0400

    quick fix

--- eclipse2526/school26/src/dinosaurs/DinoDriver.java
+++ eclipse2526/school26/src/dinosaurs/DinoDriver.java
@@ -7,19 +7,22 @@ public class DinoDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double largest = 0;
+		double avg = 0;
 		int placement = 0;
 		Dino [] dinos = new Dino[50];
 		for(int k = 0; k<dinos.length; k++) {
 			dinos[k] = new Dino();
 			System.out.println(dinos[k]+ "\n");
+			avg += dinos[k].getLength();
 			if(dinos[k].getLength() > largest) {
 				largest = dinos[k].getLength();
 				placement = k;
 			}
 		}
+		avg = avg/50;
 		System.out.println("\n largest: \n");
 		System.out.println(dinos[placement]);
-		
+		System.out.println("\n avg length is: " + avg);
 	}
 
 }
