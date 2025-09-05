55a3d5dbf03242cee4d980c4948d5840d9ea806d
J3inx <secret.atent.man@gmail.com>
2025-09-05 14:02:00 -0400

    fixed a bug on my part and made it output to a file

--- eclipse2526/school26/src/classGrades/analyzerDriver.java
+++ eclipse2526/school26/src/classGrades/analyzerDriver.java
@@ -1,5 +1,7 @@
 package classGrades;
-
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter; 
 public class analyzerDriver {
 	private static double[] grades = {0.01, 0.02, 0.99, 0.56};
 	private static double[] gradesTwo = {0.10, 0.80, 0.99, 0.56};
@@ -15,7 +17,7 @@ public class analyzerDriver {
 		
 		for(int i = 0; i<=4; i++) {
 			for(int k = 0; k<=4; k++) {
-			students[k].addGrade((i*10));
+			students[k].addGrade(Math.random()*10);
 			}
 		}
 		gradeStatistics graded = new gradeStatistics(students);
@@ -28,6 +30,25 @@ public class analyzerDriver {
 		System.out.println("\nThe lowest performing student is " + graded.getLowestStudent());
 		System.out.println("\nThe class average is " + graded.calculateClassAverage());
 		System.out.println("\nThe class median is " + graded.calculateMedianGrade());
+		
+		 
+		      
+		      
+		    
+		 try {
+			FileWriter myObj = new FileWriter("filename.txt");
+			for(int i = 0; i<4; i++) {
+				
+				myObj.write("Student " + (i+1) + " is named " + students[i].getName() + ". Their lowest grade is " + students[i].getLowestGrade() + ". Their highest grade is " + students[i].getHighestGrade() + ". And their average grade is " + students[i].calculateAverage() + "\n");
+				
+			}
+				myObj.close();
+		      System.out.println("Successfully wrote to the file.");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
